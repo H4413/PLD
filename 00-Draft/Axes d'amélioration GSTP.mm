f6e0b8d06ce5dbaf701855c9d1df4371f57b8314
Cgg <clement.geiger@gmail.com>
2011-02-03 17:02:51 +0100

    +result brainstorming jeudi aprem : - axes d'ameliorations gstp + decisions - chaines de plus-value

---
+++ 00-Draft/Axes d'amélioration GSTP.mm
@@ -0,0 +1,44 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1296748012125" ID="Freemind_Link_1051938898" MODIFIED="1296748031003" TEXT="Axes d&apos;am&#xe9;lioration GSTP">
+<node CREATED="1296748034317" ID="_" MODIFIED="1296748143031" POSITION="right" STYLE="bubble" TEXT="Diminuer les co&#xfb;ts" VSHIFT="-50">
+<node CREATED="1296748046495" ID="Freemind_Link_1040649503" MODIFIED="1296748050402" TEXT="maintenance"/>
+<node CREATED="1296748060826" ID="Freemind_Link_1975157323" MODIFIED="1296748065120" TEXT="stock"/>
+<node CREATED="1296748065630" ID="Freemind_Link_1081673075" MODIFIED="1296748072940" TEXT="achats (PR)"/>
+<node CREATED="1296748074934" ID="Freemind_Link_431605667" MODIFIED="1296748084242" TEXT="am&#xe9;liorer les pr&#xe9;visions de consommation"/>
+</node>
+<node CREATED="1296748090455" HGAP="67" ID="Freemind_Link_1186721374" MODIFIED="1296748774465" POSITION="right" STYLE="bubble" TEXT="Am&#xe9;liorer la qualit&#xe9;" VSHIFT="-99">
+<node CREATED="1296748100215" ID="Freemind_Link_461926188" MODIFIED="1296748110860" TEXT="homog&#xe9;n&#xe9;iser le SI"/>
+<node CREATED="1296748111315" ID="Freemind_Link_1622935850" MODIFIED="1296748117907" TEXT="r&#xe9;duction des pannes"/>
+<node CREATED="1296748118425" ID="Freemind_Link_1728851489" MODIFIED="1296748126097" TEXT="meilleure planification"/>
+<node CREATED="1296748126540" ID="Freemind_Link_923669522" MODIFIED="1296748139366" TEXT="r&#xe9;duire l&apos;immobilisation du mat&#xe9;riel"/>
+</node>
+<node CREATED="1296748153212" HGAP="56" ID="Freemind_Link_736733443" MODIFIED="1296748389133" POSITION="left" STYLE="bubble" TEXT="Diminuer les d&#xe9;lais lors des attributions &#xa;de mat&#xe9;riel et les maintenances" VSHIFT="-120">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1296748298818" ID="Freemind_Link_157111059" MODIFIED="1296748770684" POSITION="left" STYLE="bubble" TEXT="Am&#xe9;liorations obtenues par l&apos;ERP" VSHIFT="112">
+<node CREATED="1296748318662" HGAP="23" ID="Freemind_Link_814776634" MODIFIED="1296748428858" TEXT="Planification" VSHIFT="-47">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1296748430695" ID="Freemind_Link_994342441" MODIFIED="1296748433341" TEXT="maintenance"/>
+<node CREATED="1296748433787" ID="Freemind_Link_455169916" MODIFIED="1296748438440" TEXT="attribution mat&#xe9;riel"/>
+</node>
+<node CREATED="1296748326848" ID="Freemind_Link_756718758" MODIFIED="1296748406730" TEXT="Statistiques">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1296748346524" ID="Freemind_Link_1327594700" MODIFIED="1296748349381" TEXT="pannes"/>
+<node CREATED="1296748349998" ID="Freemind_Link_1260980404" MODIFIED="1296748354042" TEXT="temps de maintenance"/>
+<node CREATED="1296748356770" ID="Freemind_Link_1529357459" MODIFIED="1296748362162" TEXT="utilisation matos"/>
+</node>
+<node CREATED="1296748370816" HGAP="37" ID="Freemind_Link_793983663" MODIFIED="1296748515863" TEXT="processus gestion diagnostic et pannes" VSHIFT="18"/>
+<node CREATED="1296748470640" ID="Freemind_Link_1345728945" MODIFIED="1296748511742" TEXT="saisie par le chef de chantier">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1296748484975" ID="Freemind_Link_1129434346" MODIFIED="1296748495657" TEXT="op&#xe9;rations de maintenance"/>
+<node CREATED="1296748496496" ID="Freemind_Link_1167586077" MODIFIED="1296748506075" TEXT="planning d&apos;utilisation de mat&#xe9;riel"/>
+</node>
+</node>
+<node CREATED="1296748552678" ID="Freemind_Link_1547947355" MODIFIED="1296748691704" POSITION="right" STYLE="bubble" TEXT="Am&#xe9;liorations physiques" VSHIFT="44">
+<node CREATED="1296748594827" ID="Freemind_Link_1358390265" MODIFIED="1296748651496" TEXT="Suppression de deux magasins"/>
+<node CREATED="1296748653569" ID="Freemind_Link_1300147493" MODIFIED="1296748686812" TEXT="le magasin central augmente de taille &#xa;et emploie deux personnes"/>
+<node CREATED="1296748696529" ID="Freemind_Link_1648459496" MODIFIED="1296748715322" TEXT="livraison hebdomadaire par groupes de chantiers" VSHIFT="7"/>
+</node>
+</node>
+</map>
